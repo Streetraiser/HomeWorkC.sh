57b041e9c9f85cb7d0d116cb780cdf347f18c832
Streetraiser <streetraisers@gmail.com>
2023-05-12 00:49:26 +0700

    2 home task to 2 parts

---
+++ home_tack_2.2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace home_tack_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //Задача 13: Напишите программу, которая 
+        //выводит третью цифру заданного числа 
+        //или сообщает, что третьей цифры нет.
+        {
+            Console.WriteLine("Введите число:"  );
+            int number = int.Parse(Console.ReadLine()!);
+            
+            ThirdDigitsNumber(number);
+
+            void ThirdDigitsNumber(int x)
+            {
+                if (0 < x && 100 > x)
+                {
+                    Console.WriteLine("Третьей цифры нет!");
+                }
+                else if (99 < x && 1000 > x)
+                { 
+                    Console.WriteLine("Третья цифра: " + (x%10));
+                }
+                else if (999 < x && 10000 > x)
+                {
+                    Console.WriteLine("Третья цифра: " + ((x/10)%10));
+                }
+                else if (9999 < x && 100000 > x)
+                {
+                    Console.WriteLine("Третья цифра: " + ((x/100)%10));
+                } 
+                else if (99999 < x && 1000000 > x)
+                {
+                    Console.WriteLine("Третья цифра: " + ((x/1000)%10));
+
+                }  
+                else if (999999 < x && 10000000 > x)
+                {
+                    Console.WriteLine("Третья цифра: " + ((x/10000)%10));
+
+                }
+                else if (9999999 < x && 100000000 > x)
+                {
+                    Console.WriteLine("Третья цифра: " + ((x/100000)%10));
+
+                }
+                else
+                {
+                   Console.WriteLine("Для цифр длиннее 100000000 код не прописан! "); 
+                }
+
+            }
+        }   
+    }
+}
