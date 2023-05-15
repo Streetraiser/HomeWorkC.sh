4ae059e97f29d529e2194a46391597110d291969
Streetraiser <streetraisers@gmail.com>
2023-05-16 06:39:03 +0700

    3 семинар 3 задача

---
+++ lesson3_task_3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace lesson3_task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        /*Напишите программу, которая принимает
+           на вход число (N) и выдаёт таблицу 
+           кубов чисел от 1 до N. */
+        {
+           int number = Prompt("Введите число N: ");
+           TableCubes(number);
+
+           int Prompt(string message)
+           {
+            Console.Write(message);
+            int num = Convert.ToInt32(Console.ReadLine());
+            return Math.Abs(num);         //Число по модулю, для отрицательных чисел.
+           }
+
+           void TableCubes(int number)
+           {
+               int currentNum = 1;
+               int cubeNum =1;
+               while (currentNum <= number)
+               {
+                   cubeNum = currentNum*currentNum*currentNum;
+                   Console.WriteLine(cubeNum);
+                   currentNum++;
+                 
+               }
+               
+              
+            }
+           
+        }
+    }
+}
