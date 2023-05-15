0c2846c12de9e37a04d2bd5b9427ac0b8790fe2c
Streetraiser <streetraisers@gmail.com>
2023-05-16 06:29:44 +0700

    3 семинар 1 зачача

---
+++ lesson3_task_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace lesson3_task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        /*Задача 19.Напишите программу, которая принимает на вход пятизначное '''
+        число и проверяет, является ли оно палиндромом. */
+
+
+        {
+            string fiveSignsNumber = Prompt("Введите пятизначное число: ");
+            FindPolendromNumber(fiveSignsNumber);
+
+            string Prompt(string message)
+            {
+                Console.WriteLine("Введите пятизначное число: ");
+                string  fiveSignsNumber = Console.ReadLine();
+                return fiveSignsNumber;
+            }
+          
+           
+            void FindPolendromNumber(string number)
+            {
+                        
+                if (number[0] == number[4] && number[1] ==  number[3])
+                {
+                     Console.WriteLine("Да!");
+                }
+                else //dotif (number[0] != number[4] && number[1] != number[3])
+                {
+                    Console.WriteLine("Not!");
+                }
+                        
+            }
+        }
+    }
+}
