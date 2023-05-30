ff6a6ab4f8f3391c3eab0b1b3d0c2a3118bcf80f
Streetraiser <streetraisers@gmail.com>
2023-05-31 00:59:42 +0700

    Семинар_5 задача_3

---
+++ Lesson_5-task_3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Lesson_5_task_3
+{
+    class Program
+    { //Задайте массив вещественных чисел.
+    //  Найдите разницу между максимальным и минимальным элементов массива.
+        static void Main(string[] args)
+        {
+           int[] GetArray(int size, int minValue, int maxValue)
+            {
+                int[] res = new int[size];  
+                for (int i = 0; i < size; i ++)
+                {
+                    res[i] = new Random().Next(minValue, maxValue);
+                    Console.Write($"{res[i]} ");
+                }
+                return res;
+            }
+            
+            
+            
+
+            int DiffValue(int[] arr)
+            { 
+                int diff = 0;
+                int maxEl = arr[0];
+                int minEl = arr[0];
+                for (int i = 1; i <arr.Length; i++)
+                {
+                    if (arr[i] >= maxEl)
+                    {
+                         maxEl = arr[i];
+                    }
+
+                    if (arr[i] <= minEl)
+
+                    {
+                        minEl = arr[i];
+                    }
+                }
+
+                return  diff = maxEl - minEl;
+    
+            }
+            
+            int[] array = GetArray(8, -15, 100);
+            int diffEl = DiffValue(array);
+           
+            Console.WriteLine($"  Разница максимального и минимального значений : {diffEl} ");
+        }
+    }
+}
