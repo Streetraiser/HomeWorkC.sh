2d7738e01d578e217a445f4e2f569b2c723f62d4
Streetraiser <streetraisers@gmail.com>
2023-06-14 22:00:15 +0700

    Урок 9, задание 2

---
+++ Final_task-2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Final_task_2
+{//Задайте значения M и N. Напишите программу, которая 
+ //найдёт сумму натуральных элементов в промежутке от M до N. 
+
+ //M = 1; N = 15 -> 120
+  //M = 4; N = 8. -> 30
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Prompt(string message)
+            {
+                Console.WriteLine(message);
+                int number = Convert.ToInt32(Console.ReadLine());
+                return number;
+            }
+             int numberM = Prompt("Введите число: ");
+             int numberN = Prompt("Введите число: ");
+             Console.WriteLine($"Сумма натуральных элементов в промежутке от {numberM} до {numberN} \n = {SumNum(numberM, numberN)}");
+
+             int SumNum(int m, int n)
+             {
+                if(m==n) return n;                       // условие выхода
+                else return m + SumNum(m+1, n);          // обращение к подфункции с базовым вариантом функции
+                Console.WriteLine(SumNum(m, n));
+             }
+        }
+    }
+}
